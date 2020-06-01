d7c17b671b306a731cd229480741294f9c975f18
Josué Feitosa <josue.feitosa@outlook.com.br>
2020-06-01 11:39:51 -0300

    Configuring server.ts and package.json with script dev:server

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello, NLW!' });
+  res.json({ message: 'Hello, NLW2!' });
 });
 
 app.listen(3333);
